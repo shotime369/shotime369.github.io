58131310b6c65b9a56cc9262e572dd9077381ff8
shotime369 <143595834+shotime369@users.noreply.github.com>
2024-10-23 23:12:36 +0100

    qew

--- script.js
+++ script.js
@@ -27,6 +27,11 @@ function ImageSwitcher(choices, i) {
 	const bottom = $(".bottom");
 	const shirts = $(".shirt");
 	const shoes = $(".shoes");
+	const acc = $(".acc");
+	const weapon = $(".weapon");
+	const hair = $(".hair");
+	const eyes = $(".eyes");
+	const tattoo = $(".tattoo");
 
 	const shirt_picker = new ImageSwitcher(shirts);
 	document.getElementById("shirt_button").onclick = function() { shirt_picker.Next(); };
@@ -36,8 +41,21 @@ function ImageSwitcher(choices, i) {
 
 	const shoes_picker = new ImageSwitcher(shoes);
 	document.getElementById("shoes_button").onclick = function() {shoes_picker.Next(); };
-	
 
+	const acc_picker = new ImageSwitcher(acc);
+	document.getElementById("acc_button").onclick = function() {acc_picker.Next(); };
+
+	const weapon_picker = new ImageSwitcher(weapon);
+	document.getElementById("weapon_button").onclick = function (){weapon_picker.Next(); };
+
+	const hair_picker = new ImageSwitcher(hair);
+	document.getElementById("hair_button").onclick = function() {hair_picker.Next(); };
+
+	const eyes_picker = new ImageSwitcher(eyes);
+	document.getElementById("eyes_button").onclick = function() {eyes_picker.Next(); };
+
+	const tattoo_picker = new ImageSwitcher(tattoo);
+	document.getElementById("tattoo_button").onclick = function() {tattoo_picker.Next(); };
 });
 
   $("#shirt_button").click(function(){
