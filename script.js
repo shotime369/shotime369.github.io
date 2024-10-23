4e7031ef0717b316e28340ab566d3216c99e3824
shotime369 <143595834+shotime369@users.noreply.github.com>
2024-10-23 22:11:02 +0100

    qew

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 $(document).ready(function(){
 
 function ImageSwitcher(choices, i) {
+	const show_next_image = function() {
+		if(choices){
+			if(i === (choices.length)) {
+				i = 0;
+			}
+			choices[i].style.visibility = "visible";
+		}
+	};
+	const hide_current_image = function() {
+		if(choices){
+			choices[i].style.visibility = "hidden";
+			i += 1;
+		}
+	};
 	i = 0;
 	
 	this.Next = function() {
 		hide_current_image();
 		show_next_image();
 	}
-	
-	var hide_current_image = function() {
-		if(choices){
-			choices[i].style.visibility = "hidden";
-			i += 1;
-		}
-	}
-	var show_next_image = function() {
-		if(choices){
-			if(i == (choices.length)) {
-				i = 0;
-			}
-			choices[i].style.visibility = "visible";
-		}
-	}
-}
-  
-    var bottom = $(".bottom");
-	var shirts = $(".shirt");
-	var shoes = $(".shoes");
 
-	var shirt_picker = new ImageSwitcher(shirts);
+}
+
+	const bottom = $(".bottom");
+	const shirts = $(".shirt");
+	const shoes = $(".shoes");
+
+	const shirt_picker = new ImageSwitcher(shirts);
 	document.getElementById("shirt_button").onclick = function() { shirt_picker.Next(); };
-	
-	var bottom_picker = new ImageSwitcher(bottom);
+
+	const bottom_picker = new ImageSwitcher(bottom);
 	document.getElementById("bottom_button").onclick = function() {bottom_picker.Next(); };
-  
-  	var shoes_picker = new ImageSwitcher(shoes);
+
+	const shoes_picker = new ImageSwitcher(shoes);
 	document.getElementById("shoes_button").onclick = function() {shoes_picker.Next(); };
 	
 
